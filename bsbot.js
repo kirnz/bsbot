d9b5b5fcd5793b138682ac716b77563041c03595
- <76801064+kirnz@users.noreply.github.com>
2021-02-12 19:14:31 +1300

    Update bsbot.js

--- bsbot.js
+++ bsbot.js
@@ -1,7 +1,30 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+        });
+    });
+})();
 
 (function() {  
 
