ffaae8ca78e9d0d9db3e8fe4c7c38cbad91f2068
- <76801064+kirnz@users.noreply.github.com>
2021-02-12 19:24:21 +1300

    Update bsbot.js

--- bsbot.js
+++ bsbot.js
@@ -1,9 +1,14 @@
+console.log("Initialising function...")
 (function() {  
-
+    console.log("We are inside the function!")
     var waitPeriod = 3650000;
+    console.log("")
 
     setTimeout(function(){
+        console.log("setTimeout function invoked")
         $('#hourlyBeatsTimerHolder > button').click();
+        console.log("Button clicked")
     }, waitPeriod);
+    console.log("Outside function")
 
 }).call(this);
