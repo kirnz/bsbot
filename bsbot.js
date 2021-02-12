f3c2aef6a8b57ff66c7a8dfc711f7f63ea1402a2
- <76801064+kirnz@users.noreply.github.com>
2021-02-12 19:18:38 +1300

    -

--- bsbot.js
+++ bsbot.js
@@ -1,31 +1,3 @@
-(function() {
-    var startingTime = new Date().getTime();
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName("head")[0].appendChild(script);
-
-    // Poll for jQuery to come into existance
-    var checkReady = function(callback) {
-        if (window.jQuery) {
-            callback(jQuery);
-        }
-        else {
-            window.setTimeout(function() { checkReady(callback); }, 20);
-        }
-    };
-
-    // Start polling...
-    checkReady(function($) {
-        $(function() {
-            var endingTime = new Date().getTime();
-            var tookTime = endingTime - startingTime;
-            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
-        });
-    });
-})();
-
 (function() {  
 
     var waitPeriod = 3650000;
---
+++ injectJquery.js
@@ -0,0 +1,3 @@
+var jq = document.createElement('script');
+jq.src = "https://code.jquery.com/jquery-3.5.1.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
